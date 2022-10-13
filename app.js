47faab2837932629a1a32d79d34508d2ac4c1012
Mauro Marzocca <57107500+mauromarzocca@users.noreply.github.com>
2022-10-13 15:38:07 +0200

    Update JS

---
+++ app.js
@@ -0,0 +1,25 @@
+document.querySelectorAll('.tab-bar').forEach(function(item) {
+    
+    let children = Array.from(item.children);
+    let circleIndicator = item.querySelector('.tab-bar__circle-indicator');
+    let lineIndicator = item.querySelector('.tab-bar__line-indicator');
+
+    if(item.classList.contains('tab-bar-vertical')) {
+        children.forEach((childItem, key) => {
+            let color = getComputedStyle(document.documentElement).getPropertyValue(`--theme-${childItem.dataset.theme}`);
+
+            lineIndicator.classList.remove('animate');
+            lineIndicator.style.marginTop = ((25 + childItem.clientHeight) * key) + "px";
+            lineIndicator.classList.add('animate');
+
+            document.querySelector('body').style.backgroundColor = color;
+            lineIndicator.style.backgroundColor = color;
+        })
+    } else {
+        children.forEach((childItem, key) => {
+            childItem.addEventListener('click', () => {
+                
+            })
+        })
+    }
+})
